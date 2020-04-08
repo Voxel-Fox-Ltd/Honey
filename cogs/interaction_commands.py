a922b652757c67f9273fe8d690fc6afa8e03cd4d
Caleb Bartlett <callum@voxelfox.co.uk>
2020-04-08 10:22:39 +0100

    Add some more interactions

--- cogs/interaction_commands.py
+++ cogs/interaction_commands.py
@@ -10,13 +10,32 @@ interaction_responses = {
         "*{author} snuggles up to {user}, hugging them tightly.*",
         "*{author} gives {user} a tight huggo.*",
         "*{author} sneaks up to {user}, tacklehugging them from behind.*",
-        "*{author} lunges at {user}, wrapping their arms aroung them tightly.*"
+        "*{author} lunges at {user}, wrapping their arms aroung them tightly.*",
+        "*{author} was excited to see {user} and hugged them tightly with a smile.*",
+        "*{user} walked out of a store when they unexpectedly received a hug from {author}.*",
+        "*{user} asked for a hug from {author} and got what they wanted.*",
+        "*{author} sneakily walked up behind {user} and gave them a fluffy hug*",
+        "*As {user} talked, {author} went up to them and hugged them.*",
+        "*{author} ran up to a friend, {user}, and gave them a firm hug.*",
+        "*{user} wasn't suspecting anything at first, but then suddenly {author} appears and hugs them!*",
+        "*{user} felt depressed until {author} came up and gave them a friendly hug.*",
     ],
     "pat": [
         "*{author} leans forward, now looking down on {user} and softly pats their head.*",
         "*{author} decided today was a good day for pats. {user}, you're up!*",
         "*{author} jumps around until they find themselves patting {user}. Cute!*",
         "*It looks like {user} was begging for pats! So {author} gave them one.*",
+        "*{author} give {user} some pats to cheer them up.*",
+        "*{author} looks over at {user}, who is rolling around and begging for pats.*",
+        "*{user} happily nuzzles into {author}'s hand and bleps as {author} gives them lots of pats.*",
+        "*{author} give {user} some pats to cheer them up.*",
+        "*{author} graces {user}'s head with gentle pats.*",
+        "*{user} makes nonstop animal noises until {author} gives them pats.*",
+        "*{user} rolls over to {author} and gets some nice pats.*",
+        "*{author} gives {user} official pats of approval!*",
+        "*{user} flops onto {author}'s lap for gentle adorable pats.*",
+        "*{user} rolls over to {author} and gets some nice pats.*",
+        "*{user} happily nuzzles into {author}'s hand and bleps as {author} gives them lots of pats.*",
     ],
     "lick": [
         "*{author} zooms over to {user} and gives them a lick.*",
@@ -41,6 +60,30 @@ interaction_responses = {
         "*{user} was pointing to their snoot and {author} decided to boop it.*",
         "*{author} was admiring the look of {user}'s snoot, so much so they decided boop it.*",
         "*{user} felt the paw of {author} touching their nose. Embarrassed after realising they got booped.*",
+    ],
+    "hold": [
+        "*\"I'm never letting you go\", {author} says to {user} while holding them.*",
+        "*{author} holds {user} with a deep passionate smile.*",
+        "*{author} holds {user} tightly saying, \"I'm never letting go\".*",
+    ],
+    "nuzzle": [
+        "*{author} lays down with {user}, nuzzling after a hard day.*",
+        "*{author} and {user} relax together, nuzzling each other in a loving embrace.*",
+        "*{author} nuzzles {user} as they watch a romantic movie.*",
+        "*{author} nuzzles {user} affectionately to keep warm on a cold night. How adorable~!*",
+        "*{author} nuzzles {user} in bed before they fell asleep.*",
+        "*{author} sits down with {user}, nuzzling them in their lap.*",
+        "*{author} nuzzles {user} with passionate love as they watch the sunset.*",
+    ],
+    "pounce": [
+        "*{author} swoops down on {user} giving them a big surprise!*",
+        "*{author} looks at {user} and pounce on them with a very smirky smile.*",
+        "*{author} pounces {user} on their bed.*",
+        "*{author} knocks over {user} with a powerful pounce, but to {author}'s surprise, {user} retaliates with a pounce of their own!*",
+        "*Unexpectedly, {author} pounces on {user} making {author} fall flat into the snow!*",
+        "*{author} pounces on {user}; \"tag, you're it!\"*",
+        "*{author} locks onto {user} and does a well-aimed pounce onto the floor!*",
+        "*{author} pounces on {user} in a happy playful mood.*",
     ],
 }
 
@@ -114,6 +157,39 @@ class InteractionCommands(utils.Cog):
     @commands.bot_has_permissions(send_messages=True)
     async def boop(self, ctx:utils.Context, user:utils.converters.NotAuthorMember):
         """Allows you to boop a user"""
+
+        return await ctx.send(random.choice(
+            interaction_responses.get(ctx.command.name)
+        ).format(author=ctx.author.mention, user=user.mention))
+
+    @commands.command(cls=utils.Command, cooldown_after_parsing=True, aliases=[])
+    @utils.cooldown.cooldown(1, 60 * 30, commands.BucketType.member, cls=utils.cooldown.RoleBasedGuildCooldown(mapping=utils.cooldown.GroupedCooldownMapping("interactions")))
+    @commands.check(command_has_responses)
+    @commands.bot_has_permissions(send_messages=True)
+    async def hold(self, ctx:utils.Context, user:utils.converters.NotAuthorMember):
+        """Allows you to hold a user"""
+
+        return await ctx.send(random.choice(
+            interaction_responses.get(ctx.command.name)
+        ).format(author=ctx.author.mention, user=user.mention))
+
+    @commands.command(cls=utils.Command, cooldown_after_parsing=True, aliases=[])
+    @utils.cooldown.cooldown(1, 60 * 30, commands.BucketType.member, cls=utils.cooldown.RoleBasedGuildCooldown(mapping=utils.cooldown.GroupedCooldownMapping("interactions")))
+    @commands.check(command_has_responses)
+    @commands.bot_has_permissions(send_messages=True)
+    async def nuzzle(self, ctx:utils.Context, user:utils.converters.NotAuthorMember):
+        """Allows you to nuzzle a user"""
+
+        return await ctx.send(random.choice(
+            interaction_responses.get(ctx.command.name)
+        ).format(author=ctx.author.mention, user=user.mention))
+
+    @commands.command(cls=utils.Command, cooldown_after_parsing=True, aliases=[])
+    @utils.cooldown.cooldown(1, 60 * 30, commands.BucketType.member, cls=utils.cooldown.RoleBasedGuildCooldown(mapping=utils.cooldown.GroupedCooldownMapping("interactions")))
+    @commands.check(command_has_responses)
+    @commands.bot_has_permissions(send_messages=True)
+    async def pounce(self, ctx:utils.Context, user:utils.converters.NotAuthorMember):
+        """Allows you to pounce a user"""
 
         return await ctx.send(random.choice(
             interaction_responses.get(ctx.command.name)
