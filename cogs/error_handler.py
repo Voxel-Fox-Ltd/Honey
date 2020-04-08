42019230b2aefe7ac7bb4f709afc3a98a766e1fd
Caleb Bartlett <callum@voxelfox.co.uk>
2020-04-08 02:51:39 +0100

    Throw MissingPermissions if there's no moderator role set

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -64,10 +64,7 @@ class ErrorHandler(utils.Cog):
 
         # Not a guild moderator or has `manage_messages` perms
         elif isinstance(error, utils.errors.NotGuildModerator):
-            if error.guild_moderator_role == None:
-                return await ctx.send(f"This command can only be run by a guild moderator - `None`")
-            else:
-                return await ctx.send(f"This command can only be run by a guild moderator - `{error.guild_moderator_role}`")
+            return await ctx.send(f"This command can only be run by a guild moderator (role `{error.guild_moderator_role.name}`).")
 
         # NSFW channel
         elif isinstance(error, commands.NSFWChannelRequired):
--- cogs/utils/checks/is_guild_moderator.py
+++ cogs/utils/checks/is_guild_moderator.py
@@ -3,41 +3,35 @@ from discord.ext import commands
 
 
 class NotGuildModerator(commands.CommandError):
-    """Thrown when the user doesn't have `manage_message` permission or the guild moderator role."""
+    """Thrown when the user doesn't have the guild moderator role."""
 
-    def __init__(self, guild_moderator_role):
-        self.guild_moderator_role = guild_moderator_role or None
-        self.default_permission = "manage_message"
-        if self.guild_moderator_role == None:
-            message = 'You need the `{0}` permission or the guild moderator role ({1})!'.format(self.default_permission, self.guild_moderator_role)
-        else:
-            message = 'You need the `{0}` permission or the guild moderator role (`None`)!'.format(self.default_permission)
-        super().__init__(message)
+    def __init__(self, guild_moderator_role:discord.Role):
+        self.guild_moderator_role = guild_moderator_role
 
 
-def is_guild_moderator_predicate(ctx:commands.Context):
-    """Returns True if the the user has manage_message perm or has a guild moderator role"""
+def is_guild_moderator_predicate(ctx:commands.Context) -> bool:
+    """Returns True if the the user has manage_messages perm or has a guild moderator role"""
 
     if ctx.author.guild_permissions.manage_messages:
         return True
-    
-    if ctx.bot.guild_settings[ctx.guild.id]["guild_moderator_role_id"] == None:
+    if ctx.bot.guild_settings[ctx.guild.id].get("guild_moderator_role_id", None) is None:
         return False
-
-    if ctx.bot.guild_settings[ctx.guild.id]["guild_moderator_role_id"] in ctx.author._roles:
+    if ctx.bot.guild_settings[ctx.guild.id].get("guild_moderator_role_id", None) in ctx.author._roles:
         return True
+    return False
 
 
 def is_guild_moderator():
-    """Checks whether the user has manage_message perm or has a guild moderator role"""
+    """Checks whether the user has manage_messages perm in the guild or has a guild moderator role"""
 
     async def predicate(ctx:commands.Context):
 
         if is_guild_moderator_predicate(ctx):
             return True
         mod_role_id = ctx.bot.guild_settings[ctx.guild.id].get("guild_moderator_role_id")
-        if mod_role_id is not None:
-            raise NotGuildModerator(ctx.guild.get_role(mod_role_id))
-        raise NotGuildModerator(None)
+        mod_role = ctx.guild.get_role(mod_role_id)
+        if mod_role is not None:
+            raise NotGuildModerator(mod_role)
+        raise commands.MissingPermissions(["manage_messages"])
 
     return commands.check(predicate)
