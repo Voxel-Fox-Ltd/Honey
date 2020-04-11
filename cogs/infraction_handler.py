b475b5472ca4bae6c0f1397e2ed0b052cf4f1944
Caleb Bartlett <callum@voxelfox.co.uk>
2020-04-11 17:16:36 +0100

    Add infractions command

--- cogs/infraction_handler.py
+++ cogs/infraction_handler.py
@@ -4,8 +4,22 @@ from datetime import datetime as dt
 
 import discord
 from discord.ext import commands
+from discord.ext import menus
 
 from cogs import utils
+
+
+class InfractionSource(menus.ListPageSource):
+
+    def format_page(self, menu:menus.Menu, entries:list) -> utils.Embed:
+        """Format the infraction entries into an embed"""
+
+        with utils.Embed(use_random_colour=True) as embed:
+            for row in entries:
+                # TODO add timestamp
+                embed.add_field(f"{row['infraction_type']} - {row['infraction_id']}", f"<@{row['moderator_id']}> :: {row['infraction_reason']}", inline=False)
+            embed.set_footer(f"Page {menu.current_page + 1}/{self.get_max_pages()}")
+        return embed
 
 
 class InfractionHandler(utils.Cog):
@@ -28,7 +42,7 @@ class InfractionHandler(utils.Cog):
                 return code
 
     @utils.Cog.listener()
-    async def on_moderator_action(self, moderator:discord.Member, user:discord.User, reason:str, action:str):
+    async def on_moderation_action(self, moderator:discord.Member, user:discord.User, reason:str, action:str):
         """Looks for moderator actions being done and logs them into the relevant channel"""
 
         # Save to database
@@ -37,7 +51,7 @@ class InfractionHandler(utils.Cog):
             code = await self.get_unused_infraction_id(db)
             await db(
                 """INSERT INTO infractions (infraction_id, guild_id, user_id, moderator_id, infraction_type,
-                infraction_reason, timestamp) VALUES ($1, $2, $3, $4, $5)""",
+                infraction_reason, timestamp) VALUES ($1, $2, $3, $4, $5, $6, $7)""",
                 code, moderator.guild.id, user.id, moderator.id, action, db_reason, dt.utcnow(),
             )
 
@@ -61,9 +75,25 @@ class InfractionHandler(utils.Cog):
         except discord.Forbidden:
             pass
 
-    # @commands.group(cls=utils.Group)
-    # @commands.
-    # async def infractions(self, ctx:utils.Context):
+    @commands.group(cls=utils.Group, invoke_without_command=True)
+    @utils.checks.is_guild_moderator()
+    @commands.bot_has_permissions(send_messages=True, embed_links=True)
+    async def infractions(self, ctx:utils.Context, user:discord.Member):
+        """The parent to be able to see user infractions"""
+
+        # Make sure there was no subcommand invoked
+        if ctx.invoked_subcommand is not None:
+            return
+
+        # Grab their infractions from the database
+        async with self.bot.database() as db:
+            rows = await db("SELECT * FROM infractions WHERE guild_id=$1 AND user_id=$2 ORDER BY timestamp DESC", ctx.guild.id, user.id)
+        if not rows:
+            return await ctx.send(f"{user.mention} has no recorded infractions.")
+
+        # And pagination time babey
+        pages = menus.MenuPages(source=InfractionSource(rows, per_page=5), clear_reactions_after=True, delete_message_after=True)
+        await pages.start(ctx)
 
 
 def setup(bot:utils.Bot):
