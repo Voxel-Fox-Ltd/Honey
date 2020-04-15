cc122d3cec3f12d15b7c28ee9e9264c93dac8db2
Caleb Bartlett <callum@voxelfox.co.uk>
2020-04-16 00:34:56 +0100

    Add work command

--- cogs/economy_handler.py
+++ cogs/economy_handler.py
@@ -1,4 +1,7 @@
+import random
+
 import discord
+from discord.ext import commands
 
 from cogs import utils
 
@@ -61,6 +64,37 @@ class EconomyHandler(utils.Cog):
             )
             await db("INSERT INTO user_money (guild_id, user_id, amount) VALUES ($1, $2, 10000)", member.guild.id, member.id)
             await db.commit_transaction()
+
+    @commands.command(cls=utils.Command, enabled=False)
+    @utils.cooldown.cooldown(1, 60 * 60, commands.BucketType.member)
+    @commands.bot_has_permissions(send_messages=True)
+    @commands.guild_only()
+    async def work(self, ctx:utils.Context):
+        """Do some work to get you goin with some money wew"""
+
+        # Make sure there's money set up
+        if self.bot.guild_settings[ctx.guild.id].get('shop_message_id') is None:
+            return await ctx.send("There's no shop set up for this server, so there's no point in you getting money. Sorry :/")
+
+        # Add some money to the user
+        amount = random.randint(25, 75)
+        async with self.bot.database() as db:
+            await db.start_transaction()
+            await db(
+                "UPDATE user_money SET amount=user_money.amount+$3 WHERE guild_id=$1 AND user_id=$2",
+                ctx.guild.id, ctx.author.id, amount,
+            )
+            await db(
+                "UPDATE user_money SET amount=user_money.amount-$3 WHERE guild_id=$1 AND user_id=$2",
+                ctx.guild.id, ctx.guild.me.id, amount,
+            )
+            await db.commit_transaction()
+
+        # Make us an output
+        work_text = random.choice([
+            "Placeholder work text giving you {amount} coins xoxo"
+        ]).format(guild=ctx.guild, user=ctx.author, channel=ctx.channel, amount=amount)
+        await ctx.send(work_text)
 
 
 def setup(bot:utils.Bot):
