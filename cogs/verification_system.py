b3ddad47fc1c13347247efd193354ead90155a1f
Caleb Bartlett <callum@voxelfox.co.uk>
2020-04-24 23:21:17 +0100

    Add logging to verify cog

--- cogs/verification_system.py
+++ cogs/verification_system.py
@@ -15,9 +15,11 @@ class VerificationSystem(utils.Cog):
         # Get the role
         verified_role_id = self.bot.guild_settings[ctx.guild.id].get("verified_role_id")
         if verified_role_id is None:
+            self.logger.info(f"Verification attempt failed - no role set (G{ctx.guild.id})")
             return await ctx.send("There is no verified role set for this server.")
         verified_role = ctx.guild.get_role(verified_role_id)
         if verified_role is None:
+            self.logger.info(f"Verification attempt failed - role set is deleted (G{ctx.guild.id})")
             return await ctx.send("The verified role for this server is set to a deleted role.")
 
         # See if they're already verified
@@ -27,15 +29,18 @@ class VerificationSystem(utils.Cog):
         # Role manage
         try:
             await user.add_roles(verified_role, reason=f"Verified by {ctx.author!s}")
-        except discord.Forbidden:
+        except discord.Forbidden as e:
+            self.logger.info(f"Verification attempt failed, forbidden (G{ctx.guild.id}) - {e}")
             return await ctx.send(f"I was unable to add the verified role to {user.mention}.")
-        except discord.NotFound:
+        except discord.NotFound as e:
+            self.logger.info(f"Verification attempt failed (G{ctx.guild.id}) - {e}")
             return await ctx.send("To me it looks like that user doesn't exist :/")
 
         # Throw the reason into the database
         self.bot.dispatch("moderation_action", moderator=ctx.author, user=user, reason=None, action="Verify")
 
         # Output to chat
+        self.logger.info(f"Verified user (G{ctx.guild.id}/U{user.id})")
         return await ctx.send(f"{user.mention} has been verified.")
 
 
