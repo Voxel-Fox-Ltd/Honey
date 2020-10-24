50032a22e63cc8dda4c8fdf74421d4ab4b9ce6b5
Joshua Busacker <joshua@busacker.xyz>
2020-10-25 00:19:49 +1100

    Create DM listener

--- cogs/mod_mail.py
+++ cogs/mod_mail.py
@@ -1,9 +1,15 @@
+
 from cogs import utils
 
 
 class ModMail(utils.Cog):
 
-    ...
+    @utils.Cog.listener("on_message")
+    async def modmail_handler(self, message):
+
+        ctx = await self.bot.get_context(message, cls=utils.Context)
+        if ctx.command is not None or ctx.author.bot is True:
+            return
 
 
 def setup(bot: utils.Bot):
