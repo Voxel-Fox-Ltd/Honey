a6e22f53d52184e68ecaf6c30a283804cf083b07
Callum Bartlett <callum@voxelfox.co.uk>
2020-10-25 21:09:54 +0000

    Move role position properly

--- cogs/item_handler.py
+++ cogs/item_handler.py
@@ -11,6 +11,44 @@ from discord.ext import commands
 import voxelbotutils as utils
 
 
+async def move_role_position_below(role:discord.Role, below:discord.Role, reason:typing.Optional[str]) -> None:
+    """|coro|
+
+    Moves the current role beneath the given role, cascading down and fixing the order of all lower roles
+    """
+
+    if below.is_default() or role.is_default():
+        raise discord.errors.InvalidArgument("Cannot move default role")
+
+    if below == role:
+        return  # Save discord the extra request.
+
+    http = role._state.http
+
+    # Fetch the roles from the API, ordered by the order of the cache
+    all_guild_roles = sorted(await role.guild.fetch_roles())
+
+    # Get the order of the roles we want to append
+    roles = []
+    encounters = 0
+    for r in all_guild_roles[1:]:
+        if r.id == role.id:
+            encounters += 1
+            continue
+        if r.id == below.id:
+            encounters += 1
+            roles.append(role)
+            roles.append(r)
+        else:
+            roles.append(r)
+        if encounters == 2:
+            break
+
+    # Make and send the payload
+    payload = [{"id": z.id, "position": index} for index, z in enumerate(roles, start=1)]
+    await http.move_role_position(role.guild.id, payload, reason=reason)
+
+
 class ItemHandler(utils.Cog):
 
     def __init__(self, bot:utils.Bot):
@@ -270,7 +308,7 @@ class ItemHandler(utils.Cog):
             # Edit the role position
             try:
                 self.logger.info(f"Moving role {role.id} to position {role_position_role.position - 1} (my highest is {role.guild.me.top_role.position})")
-                await role.edit(position_below=role_position_role, reason="Update positioning")
+                await move_role_position_below(role, role_position_role, reason="Update positioning")
                 self.logger.info(f"Edited paint role position in guild (G{ctx.guild.id}/R{role.id})")
             except discord.Forbidden:
                 self.logger.error(f"Couldn't move paint role, forbidden (G{ctx.guild.id}/U{ctx.author.id})")
