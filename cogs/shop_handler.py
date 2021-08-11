e2afd3921107b3412ca78bfe00d27aa1637b4bff
Kae Bartlett <kae@voxelfox.co.uk>
2021-08-11 14:15:08 +0100

    First pass at converting shop to buttons

--- cogs/shop_handler.py
+++ cogs/shop_handler.py
@@ -150,14 +150,14 @@ class ShopHandler(vbu.Cog):
 
         # Generate embed
         coin_emoji = self.bot.guild_settings[guild.id].get("coin_emoji", None) or "coins"
-        emojis = []
+        buttons = []
         with vbu.Embed() as embed:
             for emoji, data in self.get_shop_items(guild).items():
                 item_price = self.bot.guild_settings[guild.id].get(data['price_key'], data['amount'])
                 if item_price <= 0:
                     continue
                 embed.add_field(f"{data['name']} ({emoji}) - {item_price} {coin_emoji}", data['description'], inline=False)
-                emojis.append(emoji)
+                buttons.append(vbu.Button(label=data['name'], emoji=emoji, custom_id="SHOP_PURCHASE"))
 
         # See if we need to edit the message
         try:
@@ -170,29 +170,28 @@ class ShopHandler(vbu.Cog):
 
         # Edit message
         self.logger.info(f"Updating shop message (G{guild.id})")
-        await shop_message.edit(content=None, embed=embed)
+        await shop_message.edit(
+            content=None,
+            embed=embed,
+            components=vbu.MessageComponents.add_buttons_with_rows(*buttons),
+        )
 
-        # Add reactions
-        await shop_message.clear_reactions()
-        for e in emojis:
-            await shop_message.add_reaction(e)
+    # @vbu.Cog.listener("on_raw_reaction_clear")
+    # async def shop_reaction_clear_listener(self, payload: discord.RawReactionClearEvent):
+    #     """
+    #     Pinged when all reactions are cleared from a shop message.
+    #     """
 
-    @vbu.Cog.listener("on_raw_reaction_clear")
-    async def shop_reaction_clear_listener(self, payload: discord.RawReactionClearEvent):
-        """
-        Pinged when all reactions are cleared from a shop message.
-        """
-
-        guild_settings = self.bot.guild_settings[payload.guild_id]
-        if guild_settings['shop_message_id'] != payload.message_id:
-            return
-        channel = self.bot.get_channel(payload.channel_id)
-        try:
-            message = await channel.fetch_message(payload.message_id)
-        except (discord.Forbidden, discord.NotFound, discord.HTTPException):
-            return
-        for emoji in self.get_shop_items(self.bot.get_guild(payload.guild_id)).keys():
-            await message.add_reaction(emoji)
+    #     guild_settings = self.bot.guild_settings[payload.guild_id]
+    #     if guild_settings['shop_message_id'] != payload.message_id:
+    #         return
+    #     channel = self.bot.get_channel(payload.channel_id)
+    #     try:
+    #         message = await channel.fetch_message(payload.message_id)
+    #     except (discord.Forbidden, discord.NotFound, discord.HTTPException):
+    #         return
+    #     for emoji in self.get_shop_items(self.bot.get_guild(payload.guild_id)).keys():
+    #         await message.add_reaction(emoji)
 
     @vbu.Cog.listener("on_raw_reaction_add")
     async def shop_reaction_listener(self, payload: discord.RawReactionActionEvent):
@@ -212,7 +211,7 @@ class ShopHandler(vbu.Cog):
 
         # Check the reaction they're giving
         emoji = str(payload.emoji)
-        item_data = self.get_shop_items(self.bot.get_guild(payload.guild_id)).get(emoji)
+        item_data = self.get_shop_items(guild).get(emoji)
 
         # Try and remove the reaction
         try:
@@ -267,6 +266,78 @@ class ShopHandler(vbu.Cog):
         try:
             guild_prefix = self.bot.guild_settings[payload.guild_id].get("prefix")
             await user.send(f"You just bought {item_data['quantity']}x **{item_data['emoji'] or item_data['name']}** in **{guild.name}**! You can use it with the `{guild_prefix}use {item_data['name'].lower().replace(' ', '')}` command.")
+        except (discord.Forbidden, AttributeError):
+            pass
+        self.logger.info(f"User successfully purchased item (G{payload.guild_id}/C{payload.channel_id}/U{payload.user_id}/E{payload.emoji!s})")
+        return
+
+    @vbu.Cog.listener("on_component_interaction")
+    async def shop_button_listener(self, payload: vbu.ComponentInteractionPayload):
+        """
+        Pinged when a user is trying to buy something from a shop.
+        """
+
+        # Check the reaction is on a shop message
+        guild_settings = self.bot.guild_settings[payload.guild.id]
+        if guild_settings['shop_message_id'] != payload.message.id:
+            return
+        guild = payload.guild
+        user = payload.user
+        channel = payload.channel
+
+        # Check the reaction they're giving
+        emoji = str(payload.component.emoji)
+        item_data = self.get_shop_items(guild).get(emoji)
+
+        # Not a valid reaction
+        if item_data is None or self.bot.guild_settings[guild.id].get(item_data['price_key'], item_data['amount']) <= 0:
+            try:
+                await payload.send(f"The emoji you added to the shop channel in **{guild.name}** doesn't refer to a valid shop item.", ephemeral=True)
+            except (discord.Forbidden, AttributeError):
+                pass
+            self.logger.info(f"Invalid reaction on shop message (G{payload.guild_id}/C{payload.channel_id}/U{payload.user_id}/E{payload.emoji!s})")
+            return
+
+        # Get the item price
+        item_price = self.bot.guild_settings[payload.guild_id].get(item_data['price_key'], item_data['amount'])
+        if item_price <= 0:
+            return
+
+        # Check their money
+        db = await self.bot.database.get_connection()
+        rows = await db("SELECT * FROM user_money WHERE guild_id=$1 AND user_id=$2", payload.guild_id, payload.user_id)
+        if not rows or rows[0]['amount'] < item_price:
+            await db.disconnect()  # TODO get amount from guild settings
+            try:
+                await payload.send(f"You don't have enough to purchase a **{item_data['name']}** item!", ephemeral=True)
+            except (discord.Forbidden, AttributeError):
+                pass
+            self.logger.info(f"User unable to purchase item (G{payload.guild_id}/C{payload.channel_id}/U{payload.user_id}/E{payload.emoji!s})")
+            return
+
+        # Alter their inventory
+        await db.start_transaction()
+        await db("UPDATE user_money SET amount=user_money.amount-$3 WHERE guild_id=$1 AND user_id=$2", guild.id, user.id, item_price)
+        await db("UPDATE user_money SET amount=user_money.amount+$3 WHERE guild_id=$1 AND user_id=$2", guild.id, guild.me.id, item_price)
+        await db(
+            """INSERT INTO user_inventory (guild_id, user_id, item_name, amount)
+            VALUES ($1, $2, $3, $4) ON CONFLICT (guild_id, user_id, item_name) DO UPDATE SET
+            amount=user_inventory.amount+excluded.amount""",
+            payload.guild_id, payload.user_id, item_data['name'], item_data['quantity']
+        )
+        await db.commit_transaction()
+        await db.disconnect()
+
+        # Send them a DM
+        try:
+            guild_prefix = self.bot.guild_settings[payload.guild_id].get("prefix")
+            await payload.send(
+                (
+                    f"You just bought {item_data['quantity']}x **{item_data['emoji'] or item_data['name']}**! "
+                    f"You can use it with the `{guild_prefix}use {item_data['name'].lower().replace(' ', '')}` command."
+                ),
+                ephemeral=True,
+            )
         except (discord.Forbidden, AttributeError):
             pass
         self.logger.info(f"User successfully purchased item (G{payload.guild_id}/C{payload.channel_id}/U{payload.user_id}/E{payload.emoji!s})")
